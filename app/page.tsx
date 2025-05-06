dfd55a5ff83fe27bd38dc6250cbae1708b96d9d7
nicholas489 <nickcarolopez@gmail.com>
2025-05-06 19:14:22 -0400

    Removed temporary WIP section

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,6 @@ export default function Home(props: { disableCustomTheme?: boolean }) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/tools.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div className="flex flex-col gap-3">
-          <h1 className="text-2xl font-bold">This webpage is currently under construction</h1>
-          <p className="text-md">Checkback soon for the updated version!</p>
-        </div>
-      </main> */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Box component="main"> 
           <HomeSection/>
